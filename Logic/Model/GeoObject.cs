2ed65aae5fd09ba032d11d5b17138d15770b7c85
Jürgen Punz <juergen.punz@icloud.com>
2017-04-18 12:59:39 +0200

    Logic-Library with Firebase integration

---
+++ Logic/Model/GeoObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Model
+{
+    class GeoObject
+    {
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public string name { get; set; }
+        public string is_in { get; set; }
+
+    }
+}
