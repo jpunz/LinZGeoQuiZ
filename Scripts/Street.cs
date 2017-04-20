a3ffc1f9a63ef9e478ad5b6be2ad218c0ca27714
Jürgen Punz <juergen.punz@icloud.com>
2017-04-20 12:48:21 +0200

    Import-Script for Geodata

---
+++ Scripts/Street.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    class Street
+    {
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public string name { get; set; }
+        public string is_in { get; set; }
+
+    }
+}
