01e47124bb475e4f919d25ee332eb545da535d03
Jürgen Punz <juergen.punz@icloud.com>
2017-04-20 20:03:41 +0200

    GameSettings

---
+++ LinzGeoQuiz/GameSettings.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LinzGeoQuiz
+{
+	public partial class GameSettings : ContentPage
+	{
+		public GameSettings()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+		{
+
+			LblNrQuestions.Text = ((Stepper)sender).Value.ToString();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			App.Current.MainPage = new Game();
+		}
+	}
+}
