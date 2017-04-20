a0e8443c7133cd5cb0640819f67f55a1f416cd9e
Jürgen Punz <juergen.punz@icloud.com>
2017-04-20 20:49:12 +0200

    Implemented request of streets and randomly choose them

--- LinzGeoQuiz/Game.xaml.cs
+++ LinzGeoQuiz/Game.xaml.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Logic.Database;
+using Logic.Model;
 
 namespace LinzGeoQuiz
 {
 	public partial class Game : ContentPage
 	{
+		private ICollection<GeoObject> geoObjects;
+
 		public Game()
 		{
 			InitializeComponent();
+			geoObjects = new Firebase().getStreets();
+
+			setNewStreet();
+		}
+
+		private void setNewStreet()
+		{
+			LblGeoObjectName.Text = System.Linq.Enumerable.ElementAt(geoObjects, new Random().Next(geoObjects.Count - 1)).name;
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			setNewStreet();
 		}
 	}
 }
