bc93f7d6022d2006e0e49d89ab605c0d1d72a832
cobe <at.obermayr@gmail.com>
2017-04-20 21:21:55 +0200

    added login, register and forgotPW

---
+++ LinzGeoQuiz/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LinzGeoQuiz
+{
+	public partial class Login : ContentPage
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		void Login_Clicked(object sender, System.EventArgs e)
+		{
+			Application.Current.MainPage = new MainTabbedPage();
+		}
+
+		void Register_Clicked(object sender, System.EventArgs e)
+		{
+			Application.Current.MainPage = new Register();
+		}
+
+		void ForgotPW_Clicked(object sender, System.EventArgs e)
+		{
+			Application.Current.MainPage = new ForgotPW();
+		}
+
+		void Cancel_Clicked(object sender, System.EventArgs e)
+		{
+			Application.Current.MainPage = new MainTabbedPage();
+		}
+	}
+}
