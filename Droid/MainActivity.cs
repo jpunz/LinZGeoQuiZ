6d8d71654d0a0ec92a855c71b4e1afaee07267e6
Jürgen Punz <juergen.punz@icloud.com>
2017-05-05 01:25:22 +0200

    Quiz-Functionality working now for Android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -21,7 +21,7 @@ namespace LinzGeoQuiz.Droid
 			base.OnCreate(bundle);
 
 			global::Xamarin.Forms.Forms.Init(this, bundle);
-			//Xamarin.FormsMaps.Init(this, bundle);
+			Xamarin.FormsMaps.Init(this, bundle);
 
 			LoadApplication(new App());
 		}
