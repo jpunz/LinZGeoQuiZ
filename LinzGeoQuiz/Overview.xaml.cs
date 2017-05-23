0de7f6cbb4d708f77feb846bf0d88f8a449d2ca0
cobe <at.obermayr@gmail.com>
2017-05-23 18:38:28 +0200

    Added # of Questions

--- LinzGeoQuiz/Overview.xaml.cs
+++ LinzGeoQuiz/Overview.xaml.cs
@@ -19,6 +19,7 @@ namespace LinzGeoQuiz
 			if (App.sumQuestions > 0)
 			{
 				LblAvgDeviation.Text = string.Format("Avg. Deviation: {0:0.00}km", (App.sumDistance / App.sumQuestions));
+				LblQuestionCount.Text = string.Format("Questions: {0:0}", App.sumQuestions);
 				LblGameCount.Text = string.Format("Games: {0:0}", App.sumGames);
 			}
 		}
